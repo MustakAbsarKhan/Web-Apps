4e917a759dabb5bea1e291afc29f05e847bed66e
Mustak Absar Khan <MustakHomeFolder@macs-MacBook-Pro.local>
2022-07-05 20:46:11 +0600

    Commit

--- Weather-App/Weather-App.js
+++ Weather-App/Weather-App.js
@@ -1,11 +1,52 @@
 window.addEventListener('load',()=> {
-    let long;
-    let lat;
+    let locationTimezone = document.querySelector('.location-timezone');
+    let weatherIcon = document.querySelector('.icon');
+    let degreeSection = document.querySelector('.degree-section')
+    let temperatureData = document.querySelector('.temp');
+    let temperatureSpan = document.querySelector('.degree-section span')
+    let weatherStatus = document.querySelector('.temp-Update');
+    let stateInfo = document.querySelector('.state');
+    let countryInfo = document.querySelector('.country'); 
 
-    if(navigator.geolocation){navigator.geolocation.getCurrentPosition((position)=>{
-        long = position.coords.longitude;
-        lat = position.coords.latitude;
-    })}else{
-        console.log("Please Accept The Location Access")
-    }
+    fetch(`https://ipinfo.io/json?d6a0865f4c921e`).then((location)=>{
+        return location.json();
+    }).then((locationdata)=>{
+        console.log(locationdata);
+        const {region}=locationdata;//getting the region from this api
+
+    let params = new URLSearchParams({
+        access_key : '6e6160f56e6b1deb1c0d541e4528b170',
+        query : region,
+        units: 'm'
+    })
+    fetch(`http://api.weatherstack.com/current?${params}`).then((response)=>{
+        return response.json();
+    }).then((data)=>{
+        console.log(data);
+    
+    const {temperature, weather_descriptions, weather_icons}= data.current;
+    const {localtime,country,name}= data.location;
+
+    //Set DOM Elements from API
+    locationTimezone.textContent = localtime;
+    temperatureData.textContent = temperature;
+    weatherStatus.textContent = weather_descriptions;
+    document.getElementById('icon').src= weather_icons;
+    stateInfo.textContent = name;
+    countryInfo.textContent = country;
+    
+     //convert celsius to farenheit
+    let farenheitdata = (temperature *9.0/5.0)+32.0;
+    //change celsius to farenheit
+    degreeSection.addEventListener('click', ()=>{
+        if(temperatureSpan.textContent === "°C"){
+            temperatureSpan.textContent = "°F";
+            temperatureData.textContent = farenheitdata;
+        }else{
+            temperatureSpan.textContent = "°C"
+            temperatureData.textContent = temperature;
+        }
+    })
+    })
+    })
 })
